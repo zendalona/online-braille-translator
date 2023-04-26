87ebcf74525ca8cb3533567629c811a95f3f1ad0
Jithesh M <jitheshmjithooz@gmail.com>
2023-04-26 18:16:18 +0530

    tool for changing font color is added

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -1,23 +1,63 @@
 import { editorsContext } from '@/pages'
-import React, { useContext, useEffect, useMemo } from 'react'
-import { createEditor } from 'slate'
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { createEditor, Editor, Node, path, Path, Range } from 'slate'
 import { Editable, Slate, withReact } from 'slate-react'
 import styles from '../../styles/TextEditor.module.css'
 import Toolbar from '../Toolbar/Toolbar'
 function TextEditor() {
     const textEditor = useMemo(() => withReact(createEditor()), [])
 
+    const [fontColor, setFontColor] = useState("#000000")
+
     const { text, setText, setBraille } = useContext(editorsContext)
+
 
     const handleChange = (value) => {
         setText(value)
 
     }
-    useEffect(() => {
-        console.log(text);
+
+    const renderLeaf = ({ attributes, children, leaf }) => {
+        console.log("leaf" + leaf.color);
+        if (leaf.color) {
+            return <span {...attributes} style={{ color: leaf.color }}>{children}</span>;
+        } else {
+            return <span {...attributes}>{children}</span>;
+        }
+    };
 
 
-    }, [text])
+    const editor = textEditor
+//function to change font color indication in toolbar
+    const cursorChange = () => {
+const {selection}=editor
+        const { anchor, focus } = editor.selection;
+
+        if (anchor && focus && Path.compare(anchor.path, focus.path) === 0) {
+            if (selection) {
+                const [node] = Editor.node(editor, selection);
+                const [leaf] = Editor.leaf(editor, selection);
+            
+                const marks = Editor.marks(editor, leaf.path);
+            
+                const color = marks?.color;
+                if(color)
+                setFontColor(color)
+                else
+                setFontColor("#000000")
+                console.log(color); // or update state with the color value
+              }
+
+            
+        }
+        else {
+            setFontColor("")
+        }
+
+
+    }
+
+
 
 
 
@@ -26,13 +66,10 @@ function TextEditor() {
             <div className="col-12 col-sm-6 px-2">
 
                 <div className={styles.textEditor}>
-                    <Toolbar />
-                    {/* <div className={styles.editorIcon}><a href="#" title="Download">
-                    <i className="fas fa-download me-3"></i>
-                </a></div> */}
-                    {/* <div className= {`${styles.textField} mx-3 p-1`}></div> */}
+                    <Toolbar editor={textEditor} fontColor={fontColor} setFontColor={setFontColor} />
+
                     <Slate editor={textEditor} value={text} onChange={(value) => handleChange(value)}>
-                        <Editable className={`${styles.textField} mx-3 p-1`} />
+                        <Editable renderLeaf={renderLeaf} onSelect={() => cursorChange()} className={`${styles.textField} mx-3 p-1`} />
                     </Slate>
                     <div className={`${styles.editorFooter} px-3 py-2`} >
                         <div><select >
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import styles from '../../styles/Toolbar.module.css'
-function Toolbar() {
+
+import { ChromePicker } from 'react-color';
+import { Editor } from 'slate';
+import { useFocused, useSelected } from 'slate-react';
+function Toolbar({editor,fontColor,setFontColor}) {
+  
+  const [showColorPicker, setShowColorPicker] = useState(false)
+
+  const fontColorChange=(color)=>{
+    setFontColor(color.hex)
+    console.log(color);
+    Editor.addMark(editor, 'color', color.hex);
+
+  }
+
+
+
+
+
+
+
   return (
-    <div className={`${styles.toolbox} container-fluid`}>
+
+<div className={`${styles.toolbox} container-fluid`}>
 
 
       <div className={`${styles.tools} px-3 col-12`}>
@@ -11,8 +32,16 @@ function Toolbar() {
           <div><a href="#" title="Undo"><i className="fas fa-undo"></i></a></div>
           <div><a href="#" title="Redo"><i className="fas fa-redo"></i></a></div>
         </div>
-      </div>
+        <div className={`${styles.toolContainer} pe-1`}>
+        <div><a onClick={()=>setShowColorPicker(!showColorPicker)} title="font"><i className="fas fa-font">
+          <div className={styles.fontColor} style={{background: fontColor}}></div>
+        
 
+          </i></a></div>
+        </div>
+      
+      </div>
+      {showColorPicker && <div className={styles.colorPicker}> <ChromePicker color={fontColor} onChange={(color)=>fontColorChange(color)}/> </div>}
     </div>
   )
 }
