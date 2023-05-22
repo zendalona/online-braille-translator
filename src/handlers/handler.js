89ce029be4ff33accf79fc9f1f391d92e0b02a2e
Jithesh M <jitheshmjithooz@gmail.com>
2023-05-22 16:36:33 +0530

    error corrected

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -1,19 +1,23 @@
 const { Transforms, Editor } = require("slate");
 
-module.exports={
-    newClick:(editor)=>{
-        
+module.exports = {
+    newClick: (editor) => {
+
         Transforms.delete(editor, {
             at: {
-              anchor: Editor.start(editor, []),
-              focus: Editor.end(editor, []),
+                anchor: Editor.start(editor, []),
+                focus: Editor.end(editor, []),
             },
-          });
-          const marks=Object.keys(Editor.marks(editor))
-          //console.log(marks);
-          marks.map((mark)=>{
-            editor.removeMark(mark)
-          })
-          
+        });
+        var marks = Editor.marks(editor)
+        if (marks) {
+            marks = Object.keys(marks)
+            //console.log(marks);
+            marks.map((mark) => {
+                editor.removeMark(mark)
+            })
+        }
+
+
     }
 }
