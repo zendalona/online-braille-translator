ecbe5d18f9b215fc11c93e6faec7c56f40a88a9c
Jithesh M <jitheshmjithooz@gmail.com>
2023-06-09 23:47:45 +0530

    find component updated

--- src/components/Find/Find.js
+++ src/components/Find/Find.js
@@ -29,7 +29,7 @@ function Find({ editor, search, setSearch, setShowFind }) {
     return (
         <Draggable cancel='.btn,i,input' bounds='parent'>
             <div className={`col-6 col-sm-4 col-xl-2 ${styles.findContainer} py-3`} style={{ boxShadow: '1px 1px 3px' }}>
-                <div className={`${styles.findHead} px-4 my-3`}><span><strong>Find</strong></span><i onClick={() => { setShowFind(false) }} className="fa fa-close" /></div>
+                <div className={`${styles.findHead} px-4 my-3`}><span><strong>Find</strong></span><i onClick={() => { setShowFind(false);setSearch(null) }} className="fa fa-close" /></div>
                 <div className={`col-12 ${styles.findBody} px-4 pe-4 my-2`}><span>Word</span><input value={search} onChange={handleChange} onKeyDown={keyPress} type="text" className="col-8" /></div>
                 {!isFound && <span style={{ display: 'flex', justifyContent: 'center' }}>Not found</span>}
                 <div className={`${styles.findFooter} px-3 my-3`}><button onClick={prevClick} className="btn btn-primary" type="button">Previous</button><button onClick={nextClick} className="btn btn-primary" type="button">Next</button></div>
