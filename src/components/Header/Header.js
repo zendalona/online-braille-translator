49bdbd20b782dc891701d1336411daec9a6eb911
Jithesh M <jitheshmjithooz@gmail.com>
2023-06-19 23:56:06 +0530

    design updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Logo from '../Logo/Logo'
-
+import styles from '../../styles/Header.module.css'
 
 function Header() {
   return (
     <>
-      <Logo />
+      <div className={styles.headerbox}>
+        <div className="pt-2 container-fluid" style={{display:"flex"}}>
+          <Logo />
+          <div className={`col-6 ${styles.headerNav} pe-5`}>
+            <a href='/'> Home</a>
+            <a href='/about'>About</a>
+          </div>
+        </div>
+      </div>
 
     </>
   )
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import styles from '../../styles/Logo.module.css'
+
 function Logo() {
   return (
-    <div className={styles.logobox}>
-      <div className="pt-2 container-fluid">
-        <h6><span >Braille Translator</span></h6>
-      </div>
-    </div>
+    
+        <div className='col-6'>
+          <h6><span >Braille Translator</span></h6>
+        </div>
+    
   )
 }
 
