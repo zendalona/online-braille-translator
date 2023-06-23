05d72189285f8a82cd7d333f7629ad69c2de8549
Jithesh M <jitheshmjithooz@gmail.com>
2023-06-23 23:49:53 +0530

    loading screen updated

--- src/components/LoadingScreen/Loading.js
+++ src/components/LoadingScreen/Loading.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '../../styles/Loading.module.css'
 
 function Loading() {
+    const [text, setText] = useState("Translating....")
+    
+    useEffect(()=>{
+        const interval= setInterval(() => {
+           setText((prev)=>prev==='Translating....'?'Translating...':'Translating....')
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+          };
+    },[])
     return (
-        <div className={styles.loading}>
-            <span className={`col-10 ${styles.loadingText}`}>
-                <span style={{ color: 'rgb(255, 255, 255)' }}>Translating....</span>
+        <div className={styles.loading} aria-live="assertive">
+            <span className={`col-10 ${styles.loadingText}`} aria-atomic="true">
+                <span style={{ color: 'rgb(255, 255, 255)' }} >{text}</span>
             </span>
         </div>
 
