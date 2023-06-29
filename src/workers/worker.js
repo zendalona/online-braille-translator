9b8a1263075590836186759636ed999aed03e617
Jithesh M <jitheshmjithooz@gmail.com>
2023-06-29 13:48:19 +0530

    performance improved

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -27,20 +27,20 @@ self.onmessage = (event) => {
     var brailleText = event.data.brailleText
     var limit = event.data.lineLimit
     console.log(limit);
-    var result = brailleText.split('')
-    if (result.length > limit) {
-        for (var i = limit; i < result.length; i = i + limit + 1) {
-            result.splice(i, 0, '\n');
+    var result = ""
+    var charCount = 0
+    for(c of brailleText){
+        if(charCount == limit){
+            result=result+"\n"
+            charCount = 1
+        }else{
+            
+            charCount++
         }
+        result = result + c
+        
     }
-    result = result.join('')
-    //console.log(result);
-    // var index = 0;
-    // var datas = []
-    // while (index < result.length) {
-    //     datas.push(result.slice(index, index + 100000))
-    //     index = index + 100000
-    // }
-    console.log("worke finish");
+    
+    console.log("work finish");
     self.postMessage(result);
 }
