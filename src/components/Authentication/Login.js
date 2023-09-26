f637a9674c5d96fd43e93ea4c751bdc09894ada0
Jithesh M <jitheshmjithooz@gmail.com>
2023-09-26 17:53:04 +0530

    login component updated

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import styles from '../../styles/Login.module.css'
 function Login() {
+    const router=useRouter()
   return (
     <section className={`container-fluid ${styles.auth_container}`}>
-  <div><a className={`btn btn-primary ${styles.signin_btn}`} href="/auth/google"><i className="fa fa-google" />SIGN IN WITH GOOGLE</a></div>
+  <div><button className={`btn btn-primary ${styles.signin_btn}`} onClick={()=>{router.push("/auth/google")}}><i className="fa fa-google" />SIGN IN WITH GOOGLE</button></div>
 </section>
 
   )
